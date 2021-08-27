5eb2366a6e32b7ae48e86c4c1fea0a528a13774c
Preet-786 <18BCE167@nirmauni.ac.in>
2021-08-27 22:30:14 +0530

    first test

--- src/main/java/com/mycompany/calculator/StringCalculator.java
+++ src/main/java/com/mycompany/calculator/StringCalculator.java
@@ -10,7 +10,10 @@ package com.mycompany.calculator;
  * @author Preet
  */
 public class StringCalculator {
-    public int Add(String numbers){       
-        return 0;
+    public int Add(String numbers){
+        if(numbers.isEmpty()){
+            return 0;
+        }
+        return Integer.parseInt(numbers);
     }
 }
--- src/test/java/com/mycompany/calculator/StringCalculatorTest.java
+++ src/test/java/com/mycompany/calculator/StringCalculatorTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.calculator;
 
 import org.junit.jupiter.api.Test;
@@ -14,16 +9,16 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class StringCalculatorTest {
     public StringCalculator CalcObj;
+    
     public StringCalculatorTest() {
         CalcObj = new StringCalculator();
     }
-
     /**
      * Test of Add method, of class StringCalculator.
      */
     @org.junit.jupiter.api.Test
     public void testAdd() {
-        assertEquals(0,CalcObj.Add("3"));
+        assertEquals(0,CalcObj.Add(""));
     }
     
 }
